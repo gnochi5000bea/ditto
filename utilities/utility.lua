7fc3743b353beaac9e1cbd0a13e0ad2f68abcb5c
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2025-07-31 17:18:04 -0700

    create_connection function

--- utilities/utility.lua
+++ utilities/utility.lua
@@ -1,1 +1,13 @@
+local services = loadstring(game:HttpGet('https://raw.githubusercontent.com/gnochi5000bea/cuck84/refs/heads/main/utilities/services.lua', true))()
 
+local utility = {connections = {}, stored_connections = {}}; do
+    function utility:create_connection(signal, callback)
+		    local connection = signal:Connect(callback)
+
+		    table.insert(utility.stored_connections, connection)
+
+		    return connection
+	  end
+end
+
+return utility
