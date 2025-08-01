a3ce0ac021c29934354f012feed41da05d783eb2
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2025-07-31 17:11:21 -0700

    is_character_loaded function

--- utilities/functions.lua
+++ utilities/functions.lua
@@ -1,1 +1,15 @@
+local services = loadstring(game:HttpGet('https://raw.githubusercontent.com/gnochi5000bea/cuck84/refs/heads/main/utilities/services.lua', true))()
 
+local lplr = services['Players'].LocalPlayer
+
+local functions = {}; do
+    function functions:is_character_loaded()
+        local character = lplr.Character
+
+        if character then 
+            return character:FindFirstChild("HumanoidRootPart"), character:FindFirstChild("Humanoid"), character
+        end
+
+        return
+    end
+end
