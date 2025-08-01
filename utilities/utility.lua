8fc872ca594c4dd3dce0c8c5131f504b9159128e
cunning <76720185+gnochi5000bea@users.noreply.github.com>
2025-07-31 17:32:59 -0700

    register_debounce, debounce functions

--- utilities/utility.lua
+++ utilities/utility.lua
@@ -1,13 +1,33 @@
 local services = loadstring(game:HttpGet('https://raw.githubusercontent.com/gnochi5000bea/cuck84/refs/heads/main/utilities/services.lua', true))()
 
-local utility = {connections = {}, stored_connections = {}}; do
+local utility = {connections = {}, stored_connections = {}, debounces = {}}; do
+    local connections = utility.connections
+    local stored_connections = utility.stored_connections
+    local debounces = utility.debounces
+    
     function utility:create_connection(signal, callback)
-		    local connection = signal:Connect(callback)
+	    local connection = signal:Connect(callback)
 
-		    table.insert(utility.stored_connections, connection)
+	    table.insert(utility.stored_connections, connection)
 
-		    return connection
-	  end
+	    return connection
+    end
+
+    function utility:register_debounce(flag)
+        debounces[flag] = tick()
+
+        return debounces[flag]
+    end
+
+    function utility:debounce(flag, time)
+        if tick() - debounces[flag] >= time then
+            debounces[flag] = tick()
+
+            return true
+        end
+
+        return false
+    end
 end
 
 return utility
